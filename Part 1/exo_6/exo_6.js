5d1fbf72c91f7df81f7cb299ee16626ba65d6067
Vivi988 <112839375+Vivi988@users.noreply.github.com>
2022-09-13 17:27:29 +1100

    Part2_exo_2

---
+++ Part 1/exo_6/exo_6.js
@@ -0,0 +1,6 @@
+function modz () {
+    var firstnum = document.getElementById("premier_nombre").value ;
+    var secondnum = document.getElementById("deuxieme_nombre").value ;
+
+    alert("le résultat est : " + firstnum % secondnum) ;
+}
