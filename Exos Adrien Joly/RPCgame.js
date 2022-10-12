97eb8153871ac3b5e7cb98919b4fb9896af2cc7b
Vivi988 <112839375+Vivi988@users.noreply.github.com>
2022-10-12 17:41:23 +1100

    pfc

---
+++ Exos Adrien Joly/RPCgame.js
@@ -0,0 +1,17 @@
+
+
+    var pierre = "pierre" ; 
+    var feuille = "feuille" ; 
+    var ciseaux = "ciseaux" ; 
+
+    var tableau = [pierre , feuille , ciseaux]; 
+
+    fonction comparer(choixjoueur)
+
+    function comparaison(choix){ 
+        var ordi = tableau[Math.floor(Math.random() * tableau.length)]
+        if
+
+}
+    
+
